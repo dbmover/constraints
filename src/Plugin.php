ac9113041d31fd00bfe23178746a08657e4b4dfd
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-10 16:42:04 +0200

    make abstract and move some vendor-specific functionality to plugins

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,26 +12,21 @@ namespace Dbmover\Constraints;
 use Dbmover\Core;
 use PDO;
 
-class Plugin extends Core\Plugin
+abstract class Plugin extends Core\Plugin
 {
+    public $description = 'Dropping existing constraints...';
+
     public function __invoke(string $sql) : string
     {
-        $operations = [];
         $stmt = $this->loader->getPdo()->prepare(
             "SELECT TABLE_NAME tbl, CONSTRAINT_NAME constr, CONSTRAINT_TYPE ctype
                 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
-            WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
-                AND (CONSTRAINT_CATALOG = ? OR CONSTRAINT_SCHEMA = ?)"
+            WHERE (CONSTRAINT_CATALOG = ? AND CONSTRAINT_SCHEMA = 'public') OR CONSTRAINT_SCHEMA = ?"
         );
         $stmt->execute([$this->loader->getDatabase(), $this->loader->getDatabase()]);
         while (false !== ($constraint = $stmt->fetch(PDO::FETCH_ASSOC))) {
             if (!$this->loader->shouldBeIgnored($constraint['constr'])) {
-                $this->loader->addOperation(sprintf(
-                    "ALTER TABLE %s DROP %s IF EXISTS %s CASCADE",
-                    $constraint['tbl'],
-                    $constraint['ctype'],
-                    $constraint['constr']
-                ));
+                $this->dropConstraint($constraint['tbl'], $constraint['constr'], $constraint['ctype']);
             }
         }
         if (preg_match_all("@^ALTER TABLE \w+ ADD FOREIGN KEY.*?;@ms", $sql, $matches, PREG_SET_ORDER)) {
@@ -42,5 +37,13 @@ class Plugin extends Core\Plugin
         }
         return $sql;
     }
+
+    public function __destruct()
+    {
+        $this->description = 'Recreating constraints...';
+        parent::__destruct();
+    }
+
+    protected abstract function dropConstraint(string $table, string $constraint, string $type);
 }
 
